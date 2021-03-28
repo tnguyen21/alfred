8a99efa0dee136d45de467558b594d7d737ebfa8
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-03-27 20:23:38 -0500

    add file intake

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,108 +1,39 @@
 import React from "react";
-import axios from 'axios';
+import Uppy from "@uppy/core";
+import { Dashboard } from "@uppy/react";
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+// import { Container } from "@chakra-ui/react";
 
+const uppy = new Uppy({
+  meta: { type: "video" },
+  restrictions: { maxNumberOfFiles: 1 },
+  autoProceed: true,
+});
+
+uppy.on("complete", (result) => {
+  const url = result.successful[0].uploadURL;
+  // store.dispatch({
+  //   type: "SET_USER_AVATAR_URL",
+  //   payload: { url: url },
+  // });
+});
 
 export default function UserInput() {
-  state = {
- 
-    // Initially, no file is selected
-    selectedFile: null
-  };
-  
-  // On file select (from the pop up)
-  onFileChange = event => {
-  
-    // Update the state
-    this.setState({ selectedFile: event.target.files[0] });
-  
-  };
-  
-  // On file upload (click the upload button)
-  onFileUpload = () => {
-  
-    // Create an object of formData
-    const formData = new FormData();
-  
-    // Update the formData object
-    formData.append(
-      "myFile",
-      this.state.selectedFile,
-      this.state.selectedFile.name
-    );
-  
-    // Details of the uploaded file
-    console.log(this.state.selectedFile);
-  
-    // Request made to the backend api
-    // Send formData object
-    axios.post("api/uploadfile", formData);
-  };
-  
-  // File content to be displayed after
-  // file upload is complete
-  fileData = () => {
-  
-    if (this.state.selectedFile) {
-       
-      return (
-        <div>
-          <h2>File Details:</h2>
-           
-<p>File Name: {this.state.selectedFile.name}</p>
-
-           
-<p>File Type: {this.state.selectedFile.type}</p>
-
-           
-<p>
-            Last Modified:{" "}
-            {this.state.selectedFile.lastModifiedDate.toDateString()}
-          </p>
-
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <br />
-          <h4>Choose before Pressing the Upload button</h4>
-        </div>
-      );
-    }
-  };
-  
-  render() {
-  
-    return (
-      <div>
-          <h1>
-            GeeksforGeeks
-          </h1>
-          <h3>
-            File Upload using React!
-          </h3>
-          <div>
-              <input type="file" onChange={this.onFileChange} />
-              <button onClick={this.onFileUpload}>
-                Upload!
-              </button>
-          </div>
-        {this.fileData()}
-      </div>
-    );
-  }
-}
-
-export default App;
-
-/*
-  let variable = "some string"; // this is boilerplate example
-
   return (
-    <>
-      <h1>User Input</h1>
-      <p>some random content</p>
-    </>
+    <div>
+      <Dashboard
+        uppy={uppy}
+        locale={{
+          strings: {
+            // Text to show on the droppable area.
+            // `%{browse}` is replaced with a link that opens the system file selection dialog.
+            dropHereOr: "Drop here or %{browse}",
+            // Used as the label for the link that opens the system file selection dialog.
+            browse: "browse",
+          },
+        }}
+      />
+    </div>
   );
 }
-*/
