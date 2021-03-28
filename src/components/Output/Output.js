86810b8343fb282e153a6853db961ff9a3588057
lynzoie <lynzieromerobugay@gmail.com>
2021-03-27 21:52:45 -0400

    new output

---
+++ src/components/Output/Output.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Output() {
+  let variable = "some string"; // this is boilerplate example
+  
+
+  return (
+    <>
+      <h1>Output</h1>
+      <p>Here's an example </p>
+    </>
+  );
+}
